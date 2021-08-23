ed22997203610211a50318713b1eb1f08fa449c6
Ty Foster <tyfoster@outlook.com>
2021-08-23 13:05:55 -0700

    Configures for React testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,13 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
+import type {Config} from '@jest/types'
+
+const config: Config.InitialOptions = {
     bail: true,
     clearMocks: true,
-    coverageProvider: 'v8',
-    presets: 'ts-jest',
-    testEnvironment: 'node',
+    preset: 'ts-jest',
+    slowTestThreshold: 6,
+    testEnvironment: 'jsdom',
     testMatch: ['**/*.test.tsx?'],
+    testTimeout: 10000,
 }
+
+export default config
