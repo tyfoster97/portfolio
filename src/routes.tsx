20252f909070eb091d6f2f81d372675bf305cb88
Ty Foster <tyfoster@outlook.com>
2021-08-23 17:28:04 -0700

    Updates import path

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,6 @@
 import React/*, { useState }*/ from 'react'
 import { Redirect, Route, Switch, withRouter } from 'react-router-dom'
-import { Home } from './components'
-import About from './components/About/About'
+import { About, Home } from './components'
 
 const Routes = () => {
     //const [errorMessage, setErrorMessage] = useState('')
