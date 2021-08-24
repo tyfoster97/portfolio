f9de4bcaac969b06de82500cd8f3ce575629be9d
Ty Foster <tyfoster@outlook.com>
2021-08-23 19:54:21 -0700

    Initial commit

---
+++ src/components/Projects/Project.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import {
+    Accordion,
+    AccordionSummary,
+    Grid,
+    makeStyles,
+    Typography,
+} from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import Project, {getDuration} from '../../utils/Project'
+
+const useStyles = makeStyles((theme) => ({
+
+}))
+
+const defaultProject: Project = {
+    name: 'Project',
+    start: new Date(),
+    description: 'the default project',
+    sourceUrl: 'https://github.com/tyfoster97',
+    skills: ['typing', 'Git']
+}
+
+type SummaryProps = {
+    name: string
+    duration: string
+}
+
+type Props = {
+    project: Project
+}
+
+const ProjectSummary = ({name, duration}: SummaryProps) => {
+    return (
+        <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}>
+            <Grid container
+                direction='row'
+                justifyContent='space-between'
+                alignItems='center'>
+                <Grid item>
+                    <Typography>{name}</Typography>
+                </Grid>
+                <Grid item>
+                    <Typography>{duration}</Typography>
+                </Grid>
+            </Grid>
+        </AccordionSummary>
+    )
+}
+
+const ProjectCard = ({project}: Props) => {
+    const classes = useStyles()
+    console.log(classes)
+    return (
+        <>
+            <Accordion>
+                <ProjectSummary name={project.name} duration={getDuration(project)} />
+            </Accordion>
+        </>
+    )
+}
+
+ProjectCard.defaultProps = {
+    project: defaultProject,
+}
+
+export default ProjectCard
---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProjectCard from './Project'
+
+const Projects = () => {
+    return (
+        <>
+        <ProjectCard />
+        </>
+    )
+}
+
+export default Projects
